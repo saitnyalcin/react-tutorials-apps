db98b04e858df7e4258ae3db815ed6e69b385713
steveyalcin <steven.yalcin@gmail.com>
2020-05-19 12:51:41 -0300

    Events handling page is implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import AppMain from './components/AppMain';
 import About from './courses/About';
+import Events from './courses/Events';
 import Home from './courses/Home';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App(props) {
   return (
@@ -28,12 +29,18 @@ function App(props) {
                 What is react
               </Link>
             </li>
+            <li>
+              <Link className="nav-link" to={'/events'}>
+                Events
+              </Link>
+            </li>
           </ul>
           <hr />
 
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
+            <Route exact path="/events" component={Events} />
           </Switch>
         </div>
       </Router>
---
+++ src/courses/Events.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Events = () => {
+  return (
+    <div>
+      <h3>
+        React events are named using camelCase, rather than lowercase. With JSX
+        you pass a function as the event handler, rather than a string.
+      </h3>
+    </div>
+  );
+};
+
+export default Events;
